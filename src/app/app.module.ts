b5b3dd250cb7b0502b3ceb0a3962151b35d937e1
af4ro <1997anshul@gmail.com>
2017-06-27 08:58:06 -0700

    routing update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {HeroDetailComponent} from './hero-detail.component';
-
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes.component'
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  }
+])
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
  }
+
+
